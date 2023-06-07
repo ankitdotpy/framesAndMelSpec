27a9fae0354af4e6ffc15503a977e9d828bc351e
ankitd <dhimanankit262@gmail.com>
2023-06-07 11:25:32 +0530

    add multiprocessing

--- main.py
+++ main.py
@@ -4,11 +4,50 @@ from pathlib import Path
 import imageio.v3 as iio
 from utils import extract_frames,extract_melspec
 
-frames_in = Path('./data/frames/raw/')
-frames_out = Path('/media/ankitd/Windows-SSD/Users/Ankit/Desktop/FrameDataset/processed')
+def get_video_frames(in_path,out_path,skip=None):
+    for path in in_path.iterdir():
+        if path.is_dir():
+            video_id = 1
+            out_dir = out_path/f'{in_path.name}/{path.name}'
+            if not out_dir.exists():
+                out_dir.mkdir()
 
-audio_in = Path('./data/audio/raw/')
-audio_out = Path('/media/ankitd/Windows-SSD/Users/Ankit/Desktop/FrameDataset/processed')
+            for video in path.iterdir():
+                name = f'frame{video_id}'
+                extract_frames(video,out_dir,skip,name)
+                video_id+=1
+
+def get_mel_spec(in_path,out_path):
+    id = 1
+    for path in in_path.iterdir():
+        if path.is_dir():
+            video_id = 1
+            out_dir = out_path/f'{in_path.name}/{path.name}'
+            if not out_dir.exists():
+                out_dir.mkdir()
+
+            for video in path.iterdir():
+                name = f'melspec_{video_id}'
+                extract_melspec(video,out_dir,name)
+                video_id+=1
+            
 
 if __name__ == '__main__':
-    pass
+    frames_in = Path('./data/frames/raw/').resolve()
+    frames_out = Path('/media/ankitd/Windows-SSD/Users/Ankit/Desktop/ProcessedData/frames_processed')
+
+    audio_in = Path('./data/audio/raw/').resolve()
+    audio_out = Path('/media/ankitd/Windows-SSD/Users/Ankit/Desktop/ProcessedData/spec_processed')
+
+    skip = 1
+
+    p1 = multiprocessing.Process(target=get_video_frames,args=(frames_in,frames_out,skip))
+    p2 = multiprocessing.Process(target=get_video_frames,args=(audio_in,audio_out))
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    print('Completed processing...\nProgram exited...\n')
--- utils.py
+++ utils.py
@@ -9,62 +9,65 @@ def resize(image,size):
     return image
 
 
-def extract_frames(in_path,out_path,size=None):
+def extract_frames(in_path,out_dir,skip=None,name=None):
     '''
     Extracts frames from videos and stores them as images
     '''
 
-    for path in in_path.iterdir():
-        assert path.is_dir()
+    '''
+    args:
+    in_path: path to video file
+    out_path: path to save output
+    skip: number of frames to skip after each save
+    name: name of the save file
+    '''
 
-        video_id = 1
-        total_files = len(list(path.glob('*')))
-        frames_path = out_path/f'{path.name}'
+    frame_id = 1
+    skip_cnt = skip or 0
+    for frame in iio.imiter(in_path,plugin='pyav'):
+        if skip_cnt == 0:
+            path = out_dir/f'{name or "frame"}_{frame_id}.png'
+            try:
+                iio.imwrite(path,frame)
+            except Exception as exp:
+                print(f'FRM: Error processing {in_path}\nException occured - {exp}\n')
+                return
+            skip_cnt = skip or 0
+        else:
+            skip_cnt-=1
+        
+        
+        
 
-        if not frames_path.exists():
-            frames_path.mkdir()
-
-        for video in path.iterdir():
-            frame_id = 1
-            for frame in iio.imiter(video,plugin='pyav'):
-                frame_path = frame_path/f'/frame{video_id}_{frame_id}.jpg'
-                try:
-                    iio.imwrite(frame_path,frame)
-                    frame_id+=1
-                except Exception as exp:
-                    print(f'Something went wrong writing {video} to the disk.\nException - {exp}')
-            print(f'Finished Processing {video.parent.name/video.name}\nRemaining From Folder {total_files-video_id}\n')
-            video_id+=1
-
-def extract_melspec(in_path,out_path):
+def extract_melspec(in_path,out_dir,name=None):
     '''
     Extracts melspec from input videos and saves them as images
     '''
-    for path in in_path.iterdir():
-        if not path.is_dir():
-            continue
-        
-        assert path.is_dir()
-        spec_id = 1
-        spec_dirpath = out_path/f'{path.name}'
-        if not spec_dirpath.exists():
-            spec_dirpath.mkdir()
 
-        for vid in path.iterdir():
-            video = mp.VideoFileClip(str(vid))
-            audio = video.audio.to_soundarray()
+    '''
+    in_path: path to audio/video file
+    out_path: path to save output melspectrogram
+    name: name of the save file
+    '''
+    audio = None
 
-            audio_mono = audio.mean(axis=1)
+    if str(in_path).endswith(('.mp3','.wav','.ogg','.flac','.m4a')):
+        audio,_ = librosa.load(in_path,sr=None)
+    else:
+        video = mp.VideoFileClip(str(in_path))
+        audio = video.audio.to_soundarray()
+        audio = audio.mean(axis=1) # Mono Audio
+    
+    mel_spec = librosa.feature.melspectrogram(audio,sr=None)
+    mel_spec_db = librosa.power_to_db(mel_spec,ref=np.max)
 
-            mel_spec = librosa.feature.melspectrogram(y=audio_mono,sr=None)
+    path = out_dir/f'{name or f"melspec_{in_path.stem}"}.png'
 
-            mel_spec_db = librosa.power_to_db(mel_spec,ref=np.max)
+    try:
+        iio.imwrite(path,mel_spec_db)
+    except Exception as exp:
+        print(f'MEL: Error Processing {in_path}\nException occured - {exp}\n')
+        return
 
-            try:
-                spec_path = spec_dirpath/f'mspec_{spec_id}.jpg'
-                iio.imwrite(spec_path,mel_spec_db)
-                print(f'Finished processing {spec_path}\n')
-            except Exception as exp:
-                print(f'Error saveing MelSpec for {path.parent.name/path.name}\nException occured - {exp}\n')
-            
-            spec_id+=1
+
+
