9bef470288fd66ba125e8618be7fc3ce8dd5480a
ankitd <dhimanankit262@gmail.com>
2023-06-08 18:30:38 +0530

    add multiprocessing

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ def get_video_frames(in_path,out_path,skip=None):
     for path in in_path.iterdir():
         if path.is_dir():
             video_id = 1
-            out_dir = out_path/f'{in_path.name}/{path.name}'
+            total_files = len(list(path.glob('*')))
+            out_dir = out_path/path.name
             if not out_dir.exists():
                 out_dir.mkdir()
 
             for video in path.iterdir():
                 name = f'frame{video_id}'
                 extract_frames(video,out_dir,skip,name)
+                print(f'FRM: Processed {video}\nRemaining - {total_files-video_id}/{total_files}\n')
                 video_id+=1
 
 def get_mel_spec(in_path,out_path):
@@ -22,19 +24,21 @@ def get_mel_spec(in_path,out_path):
     for path in in_path.iterdir():
         if path.is_dir():
             video_id = 1
-            out_dir = out_path/f'{in_path.name}/{path.name}'
+            total_files = len(list(path.glob('*')))
+            out_dir = out_path/path.name
             if not out_dir.exists():
                 out_dir.mkdir()
 
-            for video in path.iterdir():
-                name = f'melspec_{video_id}'
-                extract_melspec(video,out_dir,name)
+            for file in path.iterdir():
+                # name = f'melspec_{video_id}'
+                extract_melspec(file,out_dir)
+                print(f'MEL: Processed {file}\nRemaining - {total_files-video_id}/{total_files}\n')
                 video_id+=1
             
 
 if __name__ == '__main__':
     frames_in = Path('./data/frames/raw/').resolve()
-    frames_out = Path('/media/ankitd/Windows-SSD/Users/Ankit/Desktop/ProcessedData/frames_processed')
+    frames_out = Path('/media/ankitd/Windows-SSD/Users/Ankit/Desktop/ProcessedData/frames_data')
 
     audio_in = Path('./data/audio/raw/').resolve()
     audio_out = Path('/media/ankitd/Windows-SSD/Users/Ankit/Desktop/ProcessedData/spec_processed')
@@ -42,7 +46,7 @@ if __name__ == '__main__':
     skip = 1
 
     p1 = multiprocessing.Process(target=get_video_frames,args=(frames_in,frames_out,skip))
-    p2 = multiprocessing.Process(target=get_video_frames,args=(audio_in,audio_out))
+    p2 = multiprocessing.Process(target=get_mel_spec,args=(audio_in,audio_out))
 
     p1.start()
     p2.start()
@@ -50,4 +54,6 @@ if __name__ == '__main__':
     p1.join()
     p2.join()
 
-    print('Completed processing...\nProgram exited...\n')
+    print('Program exited...\n')
+
+    get_mel_spec(audio_in,audio_out)
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import cv2
 import librosa
+import librosa.display
+import numpy as np
 import moviepy.editor as mp
 import imageio.v3 as iio
-import numpy as np
+import matplotlib.pyplot as plt
 
 def resize(image,size):
     image = cv2.resize(image,size)
@@ -26,7 +28,8 @@ def extract_frames(in_path,out_dir,skip=None,name=None):
     skip_cnt = skip or 0
     for frame in iio.imiter(in_path,plugin='pyav'):
         if skip_cnt == 0:
-            path = out_dir/f'{name or "frame"}_{frame_id}.png'
+            path = out_dir/f'{name or "frame"}_{frame_id}.jpg'
+            frame_id+=1
             try:
                 iio.imwrite(path,frame)
             except Exception as exp:
@@ -36,9 +39,6 @@ def extract_frames(in_path,out_dir,skip=None,name=None):
         else:
             skip_cnt-=1
         
-        
-        
-
 def extract_melspec(in_path,out_dir,name=None):
     '''
     Extracts melspec from input videos and saves them as images
@@ -49,25 +49,32 @@ def extract_melspec(in_path,out_dir,name=None):
     out_path: path to save output melspectrogram
     name: name of the save file
     '''
-    audio = None
-
-    if str(in_path).endswith(('.mp3','.wav','.ogg','.flac','.m4a')):
-        audio,_ = librosa.load(in_path,sr=None)
-    else:
-        video = mp.VideoFileClip(str(in_path))
-        audio = video.audio.to_soundarray()
-        audio = audio.mean(axis=1) # Mono Audio
-    
-    mel_spec = librosa.feature.melspectrogram(audio,sr=None)
-    mel_spec_db = librosa.power_to_db(mel_spec,ref=np.max)
+    audio,sr = None,None
 
     path = out_dir/f'{name or f"melspec_{in_path.stem}"}.png'
 
+    if path.exists():
+        return
+
+    if str(in_path).endswith(('.mp3','.wav','.ogg','.flac','.m4a')):
+        audio,sr = librosa.load(in_path,sr=None)
+
+    else: # else extract audio from video
+        video = mp.VideoFileClip(str(in_path))
+        audio = video.audio.to_soundarray()
+        audio = audio.mean(axis=1) # Mono Audio
+        sr = video.audio.fps
+        del video
+
+    mel_spec = librosa.feature.melspectrogram(y=audio,sr=sr)
+    mel_spec_db = librosa.power_to_db(mel_spec,ref=np.max)
+    
+    del audio
+    
     try:
-        iio.imwrite(path,mel_spec_db)
+        librosa.display.specshow(mel_spec_db,sr=sr,x_axis='time',y_axis='mel')
+        plt.savefig(path)
+        plt.close()
     except Exception as exp:
         print(f'MEL: Error Processing {in_path}\nException occured - {exp}\n')
         return
-
-
-
